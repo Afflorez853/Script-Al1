3663a5ff506603beb6a2c4798e6a916931bc8b13
Zeus <andres.felipe.florez62@gmail.com>
2024-02-19 14:54:54 -0500

    Command is running

--- Script1.sh
+++ Script1.sh
@@ -5,3 +5,14 @@
 read -p " Carpeta_Script1 " folder_name
 mkdir  "$foldegit star_name"
 
+#Ejecutar un comando en segundo plano 
+
+echo "Executing a command in the background"
+command_to_run="start /b notepad.exe"
+nohup $command_to_run > output.txt 2>&1 &
+echo "The command is running"
+
+#Esperar un momento 
+sleep 5
+
+echo "Proceso completado"
